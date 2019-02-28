2ca4bc16a5ddfba0a2c4a18ad2a74fff4667097f
wesleysliao <wesliao@iu.edu>
2019-02-28 17:26:01 -0500

    Working force data publisher

--- src/fse103_node.py
+++ src/fse103_node.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python
-print("hello")
+
+import sys
+import rospy
+from geometry_msgs.msg import Vector3
+
+from python_fse103.fse103 import FSE103
+
+def fse103node():
+    argv = sys.argv
+    rospy.myargv(argv)
+
+    serialport_name = argv[1]
+    publish_rate = argv[2]
+
+    sensor = FSE103(serialport_name)
+
+    pub = rospy.Publisher('FSE103/force', Vector3, queue_size=10)
+    rospy.init_node('fse103_node', anonymous=True)
+    rate = rospy.Rate(float(publish_rate))
+
+    while not rospy.is_shutdown():
+        sensor.read()
+        pub.publish(sensor.force_x, sensor.force_y, sensor.force_z)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        fse103node()
+    except rospy.ROSInterruptException:
+        pass
