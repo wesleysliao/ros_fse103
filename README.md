ce292b62971b7aa3a9959546b85d5b6b641a4665
Wesley Liao <wesliao@iu.edu>
2019-05-01 18:53:49 -0400

    Changed publish message type to WrenchStamped, topic to "fse103", and updated README

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 ROS Package for the Varience FSE103 force sensor, implemented in python.
 
 ## Usage:
-`rosrun ros_fse103 fse103_node.py [serialport] [publishrate]`
+`rosrun ros_fse103 fse103_node.py [serialport] [publishrate] [frame_id]`
 
-Replace `[serialport]` with your serial port device and `[publishrate]` with how fast you want data published.
+Replace `[serialport]` with your serial port device, `[publishrate]` with how fast you want data published, and `[frame_id]` with the frame you want the Wrench to be relative to.
 
 ### Example:
 
-To read from an FSE103 on /dev/ttyACM0 at 10Hz:
+To read from an FSE103 on /dev/ttyACM0 at 10Hz in the "force_sensor" frame:
 
-`rosrun ros_fse103 fse103_node.py /dev/ttyACM0 10`
+`rosrun ros_fse103 fse103_node.py /dev/ttyACM0 10 force_sensor`
 
-Data is published as a geometry_msgs/Vector3 on FSE103/force
+Data is published as a geometry_msgs/WrenchStamped on "fse103"
--- src/fse103_node.py
+++ src/fse103_node.py
@@ -2,30 +2,44 @@
 
 import sys
 import rospy
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3, Wrench, WrenchStamped
+from std_msgs.msg import Header
 
 from python_fse103.fse103 import FSE103
 
-def fse103node():
-    argv = sys.argv
-    rospy.myargv(argv)
-
-    serialport_name = argv[1]
-    publish_rate = argv[2]
-
+def fse103node(serialport_name, publish_rate, frame_id):
     sensor = FSE103(serialport_name)
 
-    pub = rospy.Publisher('FSE103/force', Vector3, queue_size=10)
+    pub = rospy.Publisher('fse103', WrenchStamped, queue_size=10)
     rospy.init_node('fse103_node', anonymous=True)
     rate = rospy.Rate(float(publish_rate))
 
+    publishcount = 0
+
     while not rospy.is_shutdown():
         sensor.read()
-        pub.publish(sensor.force_x, sensor.force_y, sensor.force_z)
+        pub.publish(
+            WrenchStamped(
+                Header(
+		    publishcount,
+                    rospy.Time.now(),
+                    frame_id),
+                Wrench(
+                    Vector3(sensor.force_x, sensor.force_y, sensor.force_z),
+                    Vector3(0,0,0))))
+
+        publishcount += 1
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        fse103node()
+        argv = sys.argv
+        rospy.myargv(argv)
+
+        serialport_name = argv[1]
+        publish_rate = argv[2]
+        frame_id = argv[3]
+
+        fse103node(serialport_name, publish_rate, frame_id)
     except rospy.ROSInterruptException:
         pass
